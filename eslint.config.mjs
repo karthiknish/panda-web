481a5206c2698954a1813acbcb0969e73980595a
Karthik <karthik.nishanth06@gmail.com>
2025-02-18 19:45:38 +0530

    i

--- eslint.config.mjs
+++
@@ -1,7 +0,0 @@
-import { FlatCompat } from "@eslint/eslintrc";
-
-const compat = new FlatCompat();
-
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
-
-export default eslintConfig;
